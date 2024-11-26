74a8d29504bfc8225242b04bbed35c414c87d7e4
Antonio Felipe <antoniofelipemoreiraf7@gmail.com>
2024-11-26 20:24:19 -0300

    retirei /.idea

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import controller.LojaController;
+import view.VisaoLoja;
 
 public class Main {
+    public static void main(String[] args) {
+        VisaoLoja view = new VisaoLoja();
+        LojaController controller = new LojaController(view);
+    }
 }
